589a42f01d6dabe8f8653a587c5c93b526663555
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-13 14:00:18 -0700

    Implemented edit and update functionality. Fix some code discrepancies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,3 @@
-# spec/rails_helper.rb
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
@@ -23,4 +21,5 @@ RSpec.configure do |config|
   config.after(:each) { DatabaseCleaner.clean }
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
 end
