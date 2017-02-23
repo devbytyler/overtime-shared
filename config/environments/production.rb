7cb0a0bea19a8ad92efdfd517afb28eee0521e04
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-23 16:57:03 -0700

    Updated production asset managemnt rules

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.
+  config.log_formatter = ::Logger::Formatter.new
   config.serve_static_assets = true
   config.assets.digest = true
 
