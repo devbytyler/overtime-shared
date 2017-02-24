b404719a3561a69a039bcd7d71bdd797a41dedc6
tylerstephens814 <tylerstephens814@gmail.com>
2017-02-23 18:14:08 -0700

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,30 @@
-@user = User.create(email: "test@test.com", 
-                    password: "asdfasdf", 
-                    password_confirmation: "asdfasdf", 
-                    first_name: "Jon", 
+
+@employee = Employee.create(email: "test@test.com",
+                    password: "asdfasdf",
+                    password_confirmation: "asdfasdf",
+                    first_name: "Jon",
                     last_name: "Snow",
-                    phone: "2085162266")
+                    phone: "2085231806")
 
-  puts "1 User created"
+puts "1 employee created"
 
-AdminUser.create(email: "admin@test.com", 
-                password: "asdfasdf", 
-                password_confirmation: "asdfasdf", 
-                first_name: "Admin", 
-                last_name: "Name",
-                phone: "2085162266")
+AdminUser.create(email: "admin@test.com",
+                  password: "asdfasdf",
+                  password_confirmation: "asdfasdf",
+                  first_name: "Admin",
+                  last_name: "Name",
+                  phone: "2085231806")
 
-  puts "1 Admin User created"
+puts "1 Admin user created"
 
-  
-
-AuditLog.create!(user_id: @user.id, status: 0, start_date: ( Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: ( Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: ( Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @employee.id, overtime_request: 2.5)
 end
 
-  puts "100 Posts have been created"
+puts "100 Posts have been created"
